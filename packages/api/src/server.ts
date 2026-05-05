7fd2280257a57231bd3bf5fa117043002421fe4b
Yoseph Deso <jossastu@gmail.com>
2026-05-05 12:56:09 +0300

    add task api  route

--- packages/api/src/server.ts
+++ packages/api/src/server.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import cors from "cors";
-
+import { taskRouter } from "./routes/tasks";
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+//API routes
+app.use("/api/tasks", taskRouter);
 
 const PORT = 3005;
 app.listen(PORT, () => {
